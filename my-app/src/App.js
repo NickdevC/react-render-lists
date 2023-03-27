d06d3ae396b2a2458bee9205d28e945d3d8e4d82
NickdevC <nick_coles@msn.com>
2023-03-27 13:54:51 +0000

    Add RenderingLists component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from "./components/StatefulGreeting";
+import RenderingLists from "./components/RenderingLists";
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Nick" age="34" /> */}
-      <StatefulGreeting />
+      <RenderingLists />
     </div>
   );
 }
