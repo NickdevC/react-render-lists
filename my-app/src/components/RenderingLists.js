4ed580485211eee2c60eaf4b28f3d913262caef3
NickdevC <nick_coles@msn.com>
2023-03-27 14:11:25 +0000

    Add list of objects and render then using .map

--- my-app/src/components/RenderingLists.js
+++ my-app/src/components/RenderingLists.js
@@ -1,9 +1,47 @@
 import React from "react"
 
 function RenderingLists() {
+    const booklist = [
+        'To Kill a Mockingbird',
+        'The Great Gatsby',
+        'The Catcher in the Rye'
+    ]
+
+    const books = [
+        {
+            title: 'To Kill a Mockingbird',
+            author: 'Harper Lee',
+            pages: '285',
+        },
+        {
+            title: 'The Great Gatsby',
+            author: 'F. Scott Fitzgerald',
+            pages: '230',
+        },
+        {
+            title: 'The Catcher in the Rye',
+            author: 'J. D. Salinger',
+            pages: '310',
+        }
+    ]
+
     return (
         <div>
-
+            {booklist.map(book => {
+                return <h2>{book}</h2>
+            })}
+            <hr />
+            {
+                books.map(book => {
+                    return (
+                        <div>
+                            <h2>{book.title}</h2>
+                            <p>{book.author}</p>
+                            <p>{book.pages}</p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
